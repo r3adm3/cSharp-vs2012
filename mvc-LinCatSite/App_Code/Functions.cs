56b570cb18ec0ea488fc71be51d5034bf68898e9
Adrian Farnell <adrian@techfrontier.co.uk>
2013-01-15 21:57:44 +0000

    Site that reads database

---
+++ mvc-LinCatSite/App_Code/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace mvc_LinCatSite.App_Code
+{
+    public class Functions
+    {
+        public string DoIT(string id)
+        {
+            //base images folder
+            string imageFolder = @"v:\tempShare\LincatImages\";
+            string imgResultFolder = "/images/";
+
+            //first open the product folder
+            imageFolder = imageFolder + id;
+            string[] files = Directory.GetFiles(imageFolder, "*.jpg");
+            
+            foreach (string file in files)
+            {
+                return imgResultFolder + id + "/" + Path.GetFileName(file);
+                break;
+            }
+
+            return imgResultFolder + "/default.jpg";
+        }
+    }
+}
---
+++ mvc-LinCatSite/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_LinCatSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+
+            var db = new db();
+
+            var products = db.Products.Take(50);
+
+            return View(products);
+        }
+
+        //
+        // GET: /Product/Details/5
+
+        public ActionResult Details(int id)
+        {
+
+            return View();
+        }
+
+        //
+        // GET: /Product/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Product/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Product/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            var db = new db();
+
+            var product = db.Products.SingleOrDefault(x => x.Id == id);
+
+
+            return View(product);
+        }
+
+        //
+        // POST: /Product/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Product/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Product/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
